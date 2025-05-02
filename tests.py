79d6dade9d3c084773df49e419e256d951286c49
Elena Silina <silena812@gmail.com>
2025-05-02 16:15:03 +0200

    ready to pass

--- tests.py
+++ tests.py
@@ -7,36 +7,18 @@ from main import BooksCollector
 # обязательно указывать префикс Test
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
-
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
-
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
-    def test_add_new_book_add_one_book_result_one(self):
+    def test_add_new_book_add_one_book_added(self):
         collector = BooksCollector()
         collector.add_new_book('Проверяем метод добавления книги')
         assert collector.books_genre == {'Проверяем метод добавления книги': ''}
 
-    def test_set_book_genre_add_comedy_True(self):
+    def test_set_book_genre_add_comedy_added(self):
         collector = BooksCollector()
         collector.books_genre['Книга для добавления жанра'] = ''
         collector.set_book_genre('Книга для добавления жанра', 'Комедии')
         assert collector.get_book_genre('Книга для добавления жанра') == 'Комедии'
 
-    def test_get_book_genre_get_horror_True(self, book_list):
+    def test_get_book_genre_get_horror_added(self, book_list):
         collector = BooksCollector()
         collector.books_genre.update(book_list)
         assert collector.get_book_genre('Это ужасная книга 1') == 'Ужасы'
@@ -49,17 +31,17 @@ class TestBooksCollector:
             ('Ужасы', ['Это ужасная книга 1', 'Это ужасная книга 2']),
             ('Детективы', ['Это детективная книга 1' , 'Это детективная книга 2'])
         ])
-    def test_get_books_with_specific_genre_get_two_cartoon_books_True(self, genre, result, book_list):
+    def test_get_books_with_specific_genre_positive_test(self, genre, result, book_list):
         collector = BooksCollector()
         collector.books_genre.update(book_list)
         assert collector.get_books_with_specific_genre(genre) == result
 
-    def test_get_books_genre_True(self, book_list):
+    def test_get_books_genre_positive_test(self, book_list):
         collector = BooksCollector()
         collector.books_genre.update(book_list)
         assert collector.get_books_genre() == book_list
 
-    def test_get_books_for_children_True(self, book_list):
+    def test_get_books_for_children_positive_test(self, book_list):
         collector = BooksCollector()
         collector.books_genre.update(book_list)
         assert collector.get_books_for_children() == [
